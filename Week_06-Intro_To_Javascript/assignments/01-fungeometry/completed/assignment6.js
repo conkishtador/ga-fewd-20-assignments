a616650c606c76fc14631fd4cca4f42b84d96a7d
Kishore Shah <kishores339@gmail.com>
2016-06-27 19:13:56 -0700

    Hacking on HW 6
    HTML:set-up
    CSS: boxes / background color
    Javascript: First function

---
+++ Week_06-Intro_To_Javascript/assignments/01-fungeometry/completed/assignment6.js
@@ -0,0 +1,13 @@
+"use strict";
+function main () {
+	var box1 = document.getElementById("box1");
+	var box2 = document.getElementById("box2");
+	var box3 = document.getElementById("box3");
+	var box4 = document.getElementById("box4");
+
+	box1.addEventListener("click", clickedbox1);
+	box2.addEventListener("click", clickedbox2);
+	box3.addEventListener("click", clickedbox3);
+	box4.addEventListener("click", clickedbo4);
+	
+}
