4c29530fa3560149815b4e7c30f072644f4b0ed5
Kishore Shah <kishores339@gmail.com>
2016-07-18 19:38:03 -0700

    To Do List
    Changes to JS main function

--- Week_07-Forms_And_Into_To_JQuery/assignments/02-todo_list/completed/todolist.js
+++ Week_07-Forms_And_Into_To_JQuery/assignments/02-todo_list/completed/todolist.js
@@ -4,7 +4,23 @@ var main = function () {
 	$("#task").on("submit", handleForm);
 };
 
-var x = main
+var handleForm = function () {
+//** implementing .val method to get the value of an inputted form item **//
+	var taskVal = $("#task").val();
+	var priorityVal = $("#priority").val();
+	var duedateVal = $("#duedate").val();
+
+
+//**
+	var 
+
+
+
+
+
+
+	return false;
+};
 
 //**don't run main function without document's HTML loaded first**//
 $(document).ready(main);
