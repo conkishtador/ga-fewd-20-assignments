a8d7b91ae57d68f5844d48cb309ae773b73aadca
Kishore Shah <kishores339@gmail.com>
2016-07-18 19:21:39 -0700

    Updates to To Do List
    Main JS function

--- Week_07-Forms_And_Into_To_JQuery/assignments/02-todo_list/completed/todolist.js
+++ Week_07-Forms_And_Into_To_JQuery/assignments/02-todo_list/completed/todolist.js
@@ -1,1 +1,10 @@
-yg
+"use strict";
+//** main function definition **//
+var main = function () {
+	$("#task").on("submit", handleForm);
+};
+
+var x = main
+
+//**don't run main function without document's HTML loaded first**//
+$(document).ready(main);
