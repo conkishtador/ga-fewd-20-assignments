49a9cae21864643ef18c97e2c5b2dc495899d2cf
Kishore Shah <kishores339@gmail.com>
2016-07-23 16:56:45 -0700

    Final Project: Build jQuery carousel

--- Week_02-Html_And_Css_Basics/assignments/myresume/completed/Final Project/JS/javascriptfinal2.js
+++ Week_02-Html_And_Css_Basics/assignments/myresume/completed/Final Project/JS/javascriptfinal2.js
@@ -1,23 +1,12 @@
 "use strict";
 
 $(document).ready(function() {
- 
   $("#mainpic").owlCarousel({
- 
-      navigation: true, // Show next and prev buttons
+      navigation: true,
       slideSpeed: 300,
       paginationSpeed: 400,
       singleItem:true
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
   });
-
 });
 
 
