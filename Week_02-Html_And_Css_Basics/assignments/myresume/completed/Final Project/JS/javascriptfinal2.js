4618e0cb5f906a55276fb4833f16e8040d6349dd
Kishore Shah <kishores339@gmail.com>
2016-07-23 15:02:38 -0700

    Added social icons to Final Project

--- Week_02-Html_And_Css_Basics/assignments/myresume/completed/Final Project/JS/javascriptfinal2.js
+++ Week_02-Html_And_Css_Basics/assignments/myresume/completed/Final Project/JS/javascriptfinal2.js
@@ -1,17 +1,24 @@
 "use strict";
 
+$(document).ready(function() {
+ 
+  $("#mainpic").owlCarousel({
+ 
+      navigation: true, // Show next and prev buttons
+      slideSpeed: 300,
+      paginationSpeed: 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
 
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
 
